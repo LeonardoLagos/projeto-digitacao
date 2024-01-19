dc2b415c9c55aca660ebf88bb5c650fc9d9fd134
Leonardo Lagos <leonardolagos1928@gmail.com>
2024-01-19 19:17:02 -0400

    migracao servidor novo.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL:"http://ec2-15-228-13-22.sa-east-1.compute.amazonaws.com:3000/"
+    baseURL:"http://ec2-18-230-187-230.sa-east-1.compute.amazonaws.com:3000/"
 })
